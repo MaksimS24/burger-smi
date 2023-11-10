339946721abb637ac8a83638b71eb6963212402f
Maksim <serebryakov1990@bk.ru>
2023-11-10 16:13:31 +0300

    update header
    update header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
 import AppHeader from "./components/app-header/app-header";
+import BurgerIngredients from "./components/burger-ingredients/burger-ingredients";
+import BurgerConstructor from "./components/burger-constructor/burger-constructor";
 
 function App() {
-  return (
-    <div className="App">
-      <AppHeader/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <AppHeader/>
+            <main>
+                <BurgerIngredients/>
+                <BurgerConstructor/>
+            </main>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/burger-constructor/burger-constructor.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const BurgerConstructor = () => {
+    return (
+        <>
+        </>
+    );
+}
+
+export default BurgerConstructor;
