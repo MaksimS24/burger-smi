c57a6d4dfaf3b9b37e2ae88ea0149c5c73751418
Maksim <serebryakov1990@bk.ru>
2023-11-26 22:18:07 +0300

    update Ingredients and Constructor
    update Ingredients and Constructor

--- src/components/burger-ingredients/ingredients/ingredients.jsx
+++ src/components/burger-ingredients/ingredients/ingredients.jsx
@@ -5,7 +5,7 @@ import {CurrencyIcon} from "@ya.praktikum/react-developer-burger-ui-components";
 import Modal from "../../modal/modal";
 import DetailsIngredient from "./details-ingredient/details-ingredient";
 
-const Ingredients = ({name, price, image}) => {
+const Ingredients = () => {
 
 
     // const bun = ingredients.filter(bun =>
@@ -22,20 +22,21 @@ const Ingredients = ({name, price, image}) => {
         <div className={styles.mainIngredient}>
             <div className={styles.ingredients}>
                 <div onClick={() => setIsOpen(true)}>
-                    <div className={styles.array}>
-                        {ingredients.map(ingredient =>
-                            <ul className={styles.ulIngredients} key={ingredient._id}>
-                                <img src={ingredient.image} alt={'burger-ingredients'}/>
-                                <li className={'text text_type_digits-default'}>{ingredient.price}
-                                    <CurrencyIcon type={"primary"}/>
+                    <ul className={styles.array}>
+                            {ingredients.map(ingredient =>
+                                <li className={styles.ulIngredients} key={ingredient._id}>
+                                    <img src={ingredient.image} alt={'burger-ingredients'}/>
+                                    <div className={'text text_type_digits-default'}>{ingredient.price}
+                                        <CurrencyIcon type={"primary"}/>
+                                    </div>
+                                    <div className={'text text_type_main-default'}>{ingredient.name}</div>
                                 </li>
-                                <li className={'text text_type_main-default'}>{ingredient.name}</li>
-                            </ul>
-                        )}
-                    </div>
-
+                            )}
+                    </ul>
                 </div>
-                {isOpen && <Modal title="Детали ингредиента" children={<DetailsIngredient/>} setIsOpen={setIsOpen}/>}
+                {isOpen && <Modal title="Детали ингредиента" children={<DetailsIngredient/>}
+                                  setIsOpen={setIsOpen}/>
+                }
             </div>
         </div>
     );
